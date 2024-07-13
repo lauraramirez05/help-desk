21467090e4c4c2f27b90bacbf17c305aeb57a311
Laura Ramirez <lauraramirez0507@gmail.com>
2024-07-12 21:35:05 -0400

    set up webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      publicPath: '/',
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 8080,
+      proxy: [
+        {
+          context: ['/api'], // requests starting with /api
+          target: 'http://localhost:3000/', // proxy to this backend server
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: '../public/index.html',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
+};
