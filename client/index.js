3f327b8c54653fa965758a713fcc8e099408c1df
Laura Ramirez <lauraramirez0507@gmail.com>
2024-07-17 18:42:10 -0400

    trying to deploy it using vercel

--- client/index.js
+++ client/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './styles/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
 import Form from './components/Form';
 import AdminPortal from './components/AdminPortal';
+import { disabledReactDevTools } from '@fvilers/disable-react-devtools';
 import Ticket from './components/Ticket';
 import FilterModal from './components/FilterModal';
 import TicketModal from './components/TicketModal';
+
+if (process.env.NODE_ENV === 'production') disabledReactDevTools;
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -26,9 +29,9 @@ const router = createBrowserRouter([
 
 root.render(
   <StrictMode>
-    {/* <RouterProvider router={router} /> */}
+    <RouterProvider router={router} />
     {/* <App /> */}
-    <AdminPortal />
+    {/* <AdminPortal /> */}
     {/* <FilterModal /> */}
     {/* <TicketModal /> */}
   </StrictMode>
