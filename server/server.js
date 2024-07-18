bb246e1f2148cadd6b53d437b23cf483a0ed5758
Laura Ramirez <lauraramirez0507@gmail.com>
2024-07-18 03:17:39 -0400

    trying to fix cors issue

--- server/server.js
+++ server/server.js
@@ -25,6 +25,11 @@ app.use(
 );
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use('/api', ticketRouter);
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
