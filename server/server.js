dfaef44919447fecb7fd80b7c16d47d730ca1b1e
Laura Ramirez <lauraramirez0507@gmail.com>
2024-07-17 23:54:14 -0400

    server

--- server/server.js
+++ server/server.js
@@ -1,36 +1,3 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const ticketRouter = require('./routes/TicketRouter');
-// const serverless = require('serverless-http');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-const app = express();
-const port = 3000;
-
-app.use(
-  cors({
-    origin: [
-      'https://help-desk-frontend-three.vercel.app',
-      'https://help-desk-backend5.vercel.app',
-    ],
-    methods: ['POST', 'GET', 'PATCH'],
-    credentials: true,
-  })
-); //if fe and be and hosted in diff domains
-app.use(express.json());
-
-//environment variables
-const DB_URL = process.env.DB_URL;
-
-//Connect Database
-mongoose
-  .connect(DB_URL)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
 // // Serve static files (built React app)
 // app.use(express.static(path.join(__dirname, '../client', '../build')));
 
@@ -61,17 +28,5 @@ app.use((error, req, res, next) => {
     message: error.message,
   });
 });
-
-// app.post('/api/submit-form', (req, res) => {
-//   console.log(req.body);
-//   res.send('Hello from the backend!');
-// });
-
-// // Export your app wrapped with serverless-http
-// module.exports.handler = serverless(app);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 
 module.exports = app;
