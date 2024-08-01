095449c16017270324014cb4f9caee2b8b9f2fc8
Laura Ramirez <105507652+lauraramirez05@users.noreply.github.com>
2024-08-01 17:49:58 -0400

    Update SearchTicket.jsx

--- client/components/SearchTicket.jsx
+++ client/components/SearchTicket.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons';
 
-const SearchTicket = ({ onSearch }) => {
+const SearchTicket = ({ onSearch, onFetch, setPage, setSearchSuccessful }) => {
   const [query, setQuery] = useState('');
 
   const handleChange = (e) => {
@@ -10,15 +10,28 @@ const SearchTicket = ({ onSearch }) => {
   };
 
   const handleClick = (e) => {
-    console.log('hitting handleclick')
-    console.log('query')
+    console.log('hitting handleclick');
+    console.log('query');
     if (e.key === 'Enter') {
       console.log('we pressed enter');
       e.preventDefault();
+
+      if (query === '') {
+        setPage(1);
+        setSearchSuccessful(true);
+        onFetch();
+      }
       if (query) {
         onSearch(query);
       }
     }
+  };
+
+  const handleCloseClick = () => {
+    setQuery('');
+    setPage(1);
+    setSearchSuccessful(true);
+    onFetch();
   };
 
   return (
@@ -30,6 +43,11 @@ const SearchTicket = ({ onSearch }) => {
         placeholder='Search by ticket code'
         onChange={handleChange}
         onKeyDown={handleClick}
+      />
+      <FontAwesomeIcon
+        className='close-btn'
+        icon={faX}
+        onClick={handleCloseClick}
       />
     </div>
   );
